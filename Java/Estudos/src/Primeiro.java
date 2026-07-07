76c2cb24fbba783b26fdc6e6f86c8228f1a21448
Fellype <fellypemarques721@gmail.com>
2026-07-07 20:47:38 -0300

    Exercicios e estudos

--- Java/Estudos/src/Primeiro.java
+++ Java/Estudos/src/Primeiro.java
@@ -5,6 +5,29 @@
     }
 }*/
 
-void main(){
+/*void main(){
     IO.println("Ola mundo");
+}*/
+
+/// Definindo algumas variaveis
+/// int idade = 15;
+/// double altura = 1.75;
+/// char sexo = 'F';
+/// 
+
+import java.util.Locale;
+
+public class Primeiro{
+    public static void main (String [] args){
+        Locale.setDefault(Locale.US);
+        System.out.print("Ola mundo! ");
+        System.out.println("Tudo bem?");
+        int teste = 15;
+        double x = 10.3568;
+        String nome = "Maria";
+        System.out.println(teste);
+        System.out.printf("Antes tinhamos %.4f%n", x);
+        System.out.println("Agora "+ x + " Temos isso");
+        System.out.printf("%s tem em conta %.2f\n",nome,x);
+    }
 }
