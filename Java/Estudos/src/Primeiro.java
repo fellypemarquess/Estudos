0cdb34589db00a360059daa3795d3eb04cd26d81
Fellype <fellypemarques721@gmail.com>
2026-07-07 09:43:00 -0300

    Organização de repositorio

---
+++ Java/Estudos/src/Primeiro.java
@@ -0,0 +1,10 @@
+/*public class Primeiro{
+
+    public static void main(String[] args){
+        System.out.println("Ola mundo");
+    }
+}*/
+
+void main(){
+    IO.println("Ola mundo");
+}
---
+++ html,css e JS/Pessoal/README.md
@@ -0,0 +1,2 @@
+# Estudos_HTML
+Um reposit´prio de estudos sobre o HTML
