0d7e79513fe16ab0d957a543e1096e8553a09598
Fellype <fellypemarques721@gmail.com>
2026-07-14 21:09:06 -0300

    Testando o if e else

---
+++ Java/Exercicios/ex005.java
@@ -0,0 +1,23 @@
+package Exercicios;
+import java.util.Scanner;
+
+public class ex005 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int hora;
+
+        System.out.print("Que horas são? ");
+        hora = sc.nextInt();
+
+        if (hora<12 && hora>=6) {
+            System.out.println("Bom dia!");
+        }
+        else if (hora>=12 && hora<18){
+            System.out.println("Boa tarde!");
+        }
+        else{
+            System.out.println("Boa noite!");
+        }
+        sc.close();
+    }
+}
